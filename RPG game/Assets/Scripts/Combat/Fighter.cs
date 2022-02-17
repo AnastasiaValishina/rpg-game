e1146693eb2fe2f1ace06dccc8e3a5a9f5a902ca
AnastasiaValishina <azhidenova@gmail.com>
2022-02-17 17:34:16 +0100

    delay between attacks

--- RPG game/Assets/Scripts/Combat/Fighter.cs
+++ RPG game/Assets/Scripts/Combat/Fighter.cs
@@ -7,10 +7,14 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 0.5f; 
+
         Transform target;
+        float timeSinceLastAttack = 0f;
 
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
             if (target == null) return;
 
             if (!GetIsInRange())
@@ -20,6 +24,16 @@ namespace RPG.Combat
             else
             {
                 GetComponent<Mover>().Cancel();
+                AttackBehaviour();
+            }
+        }
+
+        private void AttackBehaviour()
+        {
+            if (timeBetweenAttacks < timeSinceLastAttack)
+            {
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0;
             }
         }
 
@@ -37,6 +51,12 @@ namespace RPG.Combat
         public void Cancel()
         {
             target = null;
+        }
+
+        //animation event
+        void Hit()
+        {
+
         }
     }
 }
