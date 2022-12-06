1fb95aed7f4a3592d3635b2177b6895ef9e7285f
AnastasiaValishina <azhidenova@gmail.com>
2022-12-06 12:10:40 +0100

    chase distance

---
+++ RPG game/Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 5f;
+
+        private void Update()
+        {
+            if (DistanceToPlayer() < chaseDistance)
+            {
+                Debug.Log(name + " should chase");
+            }
+        }
+
+        private float DistanceToPlayer()
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            return Vector3.Distance(transform.position, player.transform.position);
+        }
+    }
+}
