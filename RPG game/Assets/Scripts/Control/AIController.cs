46f38c9e2c9d4f3fa3bc66810894fd33d7216357
AnastasiaValishina <azhidenova@gmail.com>
2023-01-29 18:07:25 +0100

    gizmos added

--- RPG game/Assets/Scripts/Control/AIController.cs
+++ RPG game/Assets/Scripts/Control/AIController.cs
@@ -37,5 +37,11 @@ namespace RPG.Control
             float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
             return distanceToPlayer < chaseDistance;
         }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);
+        }
     }
 }
