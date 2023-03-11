5284be590fbd352a61bf8be3cd074c1fcab8cdba
AnastasiaValishina <azhidenova@gmail.com>
2023-03-11 23:26:50 +0100

    saving system

--- RPG game/Assets/Scripts/Saving/SavingSystem.cs
+++ RPG game/Assets/Scripts/Saving/SavingSystem.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.IO;
-using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections.Generic;
 using System;
 
 namespace RPG.Saving
@@ -9,51 +10,54 @@ namespace RPG.Saving
     {
         public void Save(string saveFile)
         {
+            SaveFile(saveFile, CaptureState());
+        }
+
+        public void Load(string saveFile)
+        {
+            RestoreState(LoadFile(saveFile));
+        }
+
+        private void SaveFile(string saveFile, object state)
+        {
             string path = GetPathFromSaveFile(saveFile);
-            print("Saving to " + path);
+
             using (FileStream stream = File.Open(path, FileMode.Create))
             {
-                Transform playerTransform = GetPlayerTransform();
-
-                byte[] buffer = SerializeVector(playerTransform.position);
-                stream.Write(buffer, 0, buffer.Length);
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(stream, state);
             }
         }
-        public void Load(string saveFile)
+
+        private Dictionary<string, object> LoadFile(string saveFile)
         {
             string path = GetPathFromSaveFile(saveFile);
 
             using (FileStream stream = File.Open(path, FileMode.Open))
             {
-                byte[] buffer = new byte[stream.Length];
-                stream.Read(buffer, 0, buffer.Length);
-                Transform playerTransform = GetPlayerTransform();
-                playerTransform.position = DeserializeVector(buffer);
+                BinaryFormatter formatter = new BinaryFormatter();
+                return (Dictionary<string, object>)formatter.Deserialize(stream);
             }
         }
 
-        private Transform GetPlayerTransform()
+        private Dictionary<string, object> CaptureState()
         {
-            return GameObject.FindGameObjectWithTag("Player").transform;
+            Dictionary<string, object> state = new Dictionary<string, object>();
+            foreach(SaveableEntity saveable in FindObjectsOfType<SaveableEntity>())
+            {
+                state[saveable.GetUniqueIdentifier()] = saveable.CaptureState();
+            }
+            return state;
         }
 
-        private byte[] SerializeVector(Vector3 vector)
+        private void RestoreState(Dictionary<string, object> state)
         {
-            byte[] vectorBytes = new byte[3*4];
-            BitConverter.GetBytes(vector.x).CopyTo(vectorBytes, 0);
-            BitConverter.GetBytes(vector.y).CopyTo(vectorBytes, 4);
-            BitConverter.GetBytes(vector.z).CopyTo(vectorBytes, 8);
-            return vectorBytes;
+            foreach (SaveableEntity saveable in FindObjectsOfType<SaveableEntity>())
+            {
+                saveable.RestoreState(state[saveable.GetUniqueIdentifier()]);
+            }
         }
 
-        private Vector3 DeserializeVector(byte[] buffer)
-        {
-            Vector3 result = new Vector3();
-            result.x = BitConverter.ToSingle(buffer, 0);
-            result.y = BitConverter.ToSingle(buffer, 4);
-            result.z = BitConverter.ToSingle(buffer, 8);
-            return result;
-        }
         string GetPathFromSaveFile(string saveFile)
         {
             return Path.Combine(Application.persistentDataPath, saveFile + ".sav");
