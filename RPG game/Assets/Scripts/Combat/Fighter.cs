b299805de8e4e030b0fbc056d3f228a8cfcfc360
AnastasiaValishina <azhidenova@gmail.com>
2023-06-12 16:10:24 +0200

    bug attacking distant enemies

--- RPG game/Assets/Scripts/Combat/Fighter.cs
+++ RPG game/Assets/Scripts/Combat/Fighter.cs
@@ -106,6 +106,7 @@ namespace RPG.Combat
         public bool CanAttack(GameObject combatTarget)
         {
             if (combatTarget == null) return false;
+            if (!GetComponent<Mover>().CanMoveTo(combatTarget.transform.position)) return false;
             Health targetToTest = combatTarget.GetComponent<Health>();
             return targetToTest != null && !targetToTest.IsDead();
         }
--- RPG game/Assets/Scripts/Control/PlayerController.cs
+++ RPG game/Assets/Scripts/Control/PlayerController.cs
@@ -11,7 +11,6 @@ namespace RPG.Control
     {
         [SerializeField] CursorMapping[] cursorMappings;
         [SerializeField] float maxHavMeshProjectionDistance;
-        [SerializeField] float maxNavPathLength = 40f;
 
         Mover mover;
         Health health;
@@ -91,6 +90,8 @@ namespace RPG.Control
             bool hasHit = RaycastNavMesh(out target);
             if (hasHit)
             {
+                if (!mover.CanMoveTo(target)) return false;
+
                 if (Input.GetMouseButton(0))
                 {
                     mover.StartMoveAction(target, 1f);
@@ -117,25 +118,7 @@ namespace RPG.Control
 
             target = navMeshHit.position;
 
-            NavMeshPath path = new NavMeshPath();
-            bool hasPath = NavMesh.CalculatePath(transform.position, target, NavMesh.AllAreas, path);
-            if (!hasPath) return false;
-            if (path.status != NavMeshPathStatus.PathComplete) return false;
-            if (GetPathLength(path) > maxNavPathLength) return false;
-
             return true;
-        }
-
-        private float GetPathLength(NavMeshPath path)
-        {
-            float total = 0;
-
-            if (path.corners.Length < 2) return total;
-            for (int i = 0; i < path.corners.Length - 1; i++)
-            {
-                total += Vector3.Distance(path.corners[i], path.corners[i + 1]);                
-            }
-            return total;
         }
 
         private void SetCursor(CursorType type)
