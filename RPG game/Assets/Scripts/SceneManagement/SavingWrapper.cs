3620ce8ac747bb501e28b4d5579282a7f20b6625
AnastasiaValishina <azhidenova@gmail.com>
2023-10-08 09:45:36 +0200

    saving bug fix

--- RPG game/Assets/Scripts/SceneManagement/SavingWrapper.cs
+++ RPG game/Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -33,7 +33,7 @@ namespace RPG.SceneManagement
 
         public void Load()
         {
-            StartCoroutine(GetComponent<SavingSystem>().LoadLastScene(defaultSaveFile));
+            GetComponent<SavingSystem>().Load(defaultSaveFile);
         }
 
         public void Save()
